00f2c9c488f59ab796abaf6cae690ccbe68d3630
Ruslan <ruslangazizov36@gmail.com>
2021-10-14 09:09:51 +0300

    Add bad answers processing

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,21 +1,14 @@
 from aiogram import types
-from aiogram.dispatcher import FSMContext
 
+from data.constants import choose_menu_item_prompt
 from loader import dp
 
 
-# Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 @dp.message_handler(state=None)
 async def bot_echo(message: types.Message):
-    await message.answer(f"Эхо без состояния. "
-                         f"Сообщение:\n"
-                         f"{message.text}")
+    await message.answer(choose_menu_item_prompt)
 
 
-# Эхо хендлер, куда летят ВСЕ сообщения с указанным состоянием
 @dp.message_handler(state="*", content_types=types.ContentTypes.ANY)
-async def bot_echo_all(message: types.Message, state: FSMContext):
-    state = await state.get_state()
-    await message.answer(f"Эхо в состоянии <code>{state}</code>.\n"
-                         f"\nСодержание сообщения:\n"
-                         f"<code>{message}</code>")
+async def bot_echo_all(message: types.Message):
+    await message.answer(choose_menu_item_prompt)
