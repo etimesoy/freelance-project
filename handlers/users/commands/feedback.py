a4cdd91f7296a6bb25cfa30876cda50f9df1ae6f
Ruslan <ruslangazizov36@gmail.com>
2021-10-14 09:06:36 +0300

    Adjust feedback state names and take out message texts from method calls to variables

--- handlers/users/commands/feedback.py
+++ handlers/users/commands/feedback.py
@@ -18,25 +18,26 @@ async def send_welcome_message(message: types.Message, state: FSMContext):
     await state.update_data(command="feedback")
     await state.update_data(table_name="feedbacks")
     await state.update_data(user_tg_username=message.from_user.username)
-    await message.answer("""<b>Доброго времени суток!</b>
+    message_text = """<b>Доброго времени суток!</b>
 Очень рады, что Вы решили воспользоваться возможностями нашего бота и оставить отзыв 🙂
 Будем благодарны за развернутый отзыв о нашей работе! 
 
 ________________________
-<b>Пожалуйста, напишите название вашего проекта</b>""", reply_markup=types.ReplyKeyboardRemove())
-    await state.set_state("get_project_name")
+<b>Пожалуйста, напишите название вашего проекта</b>"""
+    await message.answer(message_text, reply_markup=types.ReplyKeyboardRemove())
+    await state.set_state("feedback__get_project_name")
 
 
-@dp.message_handler(state="get_project_name")
+@dp.message_handler(state="feedback__get_project_name")
 async def get_project_name(message: types.Message, state: FSMContext):
     project_name = message.text
     await state.update_data(project_name=project_name)
-    await message.answer("Отлично!\nПожалуйста, коротко оцените работу наших сотрудников",
-                         reply_markup=stars_keyboard)
-    await state.set_state("get_assessment_of_work")
+    message_text = "Отлично!\nПожалуйста, коротко оцените работу наших сотрудников"
+    await message.answer(message_text, reply_markup=stars_keyboard)
+    await state.set_state("feedback__get_assessment_of_work")
 
 
-@dp.callback_query_handler(stars_callback.filter(), state="get_assessment_of_work")
+@dp.callback_query_handler(stars_callback.filter(), state="feedback__get_assessment_of_work")
 async def get_assessment_of_work(call: types.CallbackQuery, state: FSMContext, callback_data: dict):
     await call.answer()
     await call.message.edit_reply_markup()
@@ -81,7 +82,7 @@ async def send_gratitude_response(message: types.Message, state: FSMContext):
 
     if stars_count in ["1", "2", "3"]:
         await message.answer(message_text, reply_markup=keyboard)
-        await state.set_state("get_gratitude")
+        await state.set_state("feedback__get_gratitude")
     elif stars_count in ["4", "5"]:
         await message.answer(message_text, reply_markup=main_keyboard)
         await state.reset_state()
@@ -89,16 +90,16 @@ async def send_gratitude_response(message: types.Message, state: FSMContext):
 
 
 @dp.callback_query_handler(feedback_callback.filter(action="Предложить улучшение"),
-                           state="get_gratitude")
+                           state="feedback__get_gratitude")
 async def send_suggestions_for_improvements_message(call: types.CallbackQuery, state: FSMContext):
     await call.message.edit_reply_markup()
     await call.message.answer("<b>Пожалуйста, напишите, что мы могли бы улучшить 😔</b>",
                               reply_markup=types.ReplyKeyboardRemove())
-    await state.set_state("get_suggestions_for_improvements_message")
+    await state.set_state("feedback__get_suggestions_for_improvements_message")
 
 
 @rate_limit(limit=180)
-@dp.message_handler(state="get_suggestions_for_improvements_message")
+@dp.message_handler(state="feedback__get_suggestions_for_improvements_message")
 async def get_suggestions_for_improvements_message(message: types.Message, state: FSMContext):
     suggestions_for_improvements = message.text
     # TODO: проверить, не имеет ли уже пользователь скидочный код
@@ -118,7 +119,7 @@ async def get_suggestions_for_improvements_message(message: types.Message, state
 
 @rate_limit(limit=180)
 @dp.callback_query_handler(feedback_callback.filter(action="Пропустить"),
-                           state="get_gratitude")
+                           state="feedback__get_gratitude")
 async def go_back_to_main_menu(call: types.CallbackQuery, state: FSMContext):
     await call.message.edit_reply_markup()
     state_data = await state.get_data()
