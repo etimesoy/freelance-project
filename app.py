3af5a6cf9bca5c61021d3dcc56df20631adfe57f
Ruslan <ruslangazizov36@gmail.com>
2021-10-16 13:57:40 +0300

    Add Gcloud support

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import logging
+from asyncio import get_event_loop
 
-from aiogram import executor
+from flask import Request
+
+from aiogram import executor, Bot, Dispatcher
+from aiogram import types
 
 from loader import dp, bot
 import middlewares, filters, handlers
@@ -19,3 +23,14 @@ async def on_startup(dispatcher):
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup)
+
+
+def gcloud_function_main(request: Request):
+    request_json = request.get_json()
+    Dispatcher.set_current(dp)
+    Bot.set_current(bot)
+    update = types.Update(update_id=request_json["update_id"],
+                          message=request_json["message"])
+
+    loop = get_event_loop()
+    loop.run_until_complete(dp.process_update(update))
--- loader.py
+++ loader.py
@@ -1,10 +1,12 @@
 from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 from data import config
 from utils.db_api.firebase import Database
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-storage = RedisStorage2(host='tg-redis')
+# storage = RedisStorage2(host='tg-redis')
+storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 db = Database()
