26c4b4280d91b01ab295bfae7872fa31e0fcb0cb
Ruslan <ruslangazizov36@gmail.com>
2021-10-21 22:43:42 +0300

    Add telegram request processing

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import logging
-from asyncio import get_event_loop
+import asyncio
 
 from flask import Request
 
-from aiogram import executor, Bot, Dispatcher
-from aiogram import types
+from aiogram import executor, Bot, Dispatcher, types
 
 from loader import dp, bot
 import middlewares, filters, handlers
@@ -21,16 +20,29 @@ async def on_startup(dispatcher):
     await on_startup_notify(dispatcher)
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
-
-
 def gcloud_function_main(request: Request):
-    request_json = request.get_json()
-    Dispatcher.set_current(dp)
-    Bot.set_current(bot)
-    update = types.Update(update_id=request_json["update_id"],
-                          message=request_json["message"])
+    """Google cloud functions handler."""
 
-    loop = get_event_loop()
-    loop.run_until_complete(dp.process_update(update))
+    request_json = request.get_json()
+    print(f"{request_json=}")
+
+    if request.method == 'POST':
+        try:
+            loop = asyncio.get_running_loop()
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+        loop.run_until_complete(process_event(request_json))
+        return {'statusCode': 200, 'body': 'ok'}
+
+    return {'statusCode': 405}
+
+
+async def process_event(request_json):
+    """
+    Converting an Google cloud functions event to an update and
+    handling it
+    """
+    Bot.set_current(bot)
+    Dispatcher.set_current(dp)
+    update = types.Update.to_object(request_json)
+    await dp.process_update(update)
